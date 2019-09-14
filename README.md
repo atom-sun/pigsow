7a2e7d340d5a97e3f1361dee726c8a82d2106a89
Sun Ning <sunning@flowam.com>
2019-09-14 03:58:28 +0000

    pigsow

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # pigsow
 The love of pig and sow.
+
+```bash
+git clone http://github.com/atom-sun/pigsow.git
+python3 pigsow/pigsow.py
+```
+
+```bash
+mv pigsow ~/.pigsow
+echo 'alias pigsow="python3 ~/.pigsow/pigsow.py"' >> .bashrc
+source ~/.bashrc
+pigsow
+```
+
+## Authors
+
+- **Ning Sun**
+  - email: sunning@flowam.com
+- **Pengfei Zhang**
+  - email: pzhang93@caltech.edu
---
+++ pigsow.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import datetime
+
+
+d1 = datetime.datetime(2017, 8, 17)
+d2 = datetime.datetime(2018, 6, 19)
+d3 = datetime.datetime.now()
+
+daysCount = (d3 - d1).days
+
+print('\n')
+print('=' * 50)
+print('|' + ' ' * 48 + '|')
+print(f'|    pig and sow are being in love {daysCount:=5}  days.   |')
+print('|' + ' ' * 48 + '|')
+print('=' * 50)
+print(' ' * 31 + 'v0.1  by little sow')
+print('   ＾--＾   ❤   ＾--＾ ')
+print('  / 。。\      / ^ ^ \ ')
+print(' ( (o o) )    ( (o o) )')
+print(' ' * 31 + 'v0.2  by little pig')
+print('\n')
